08d94c1c6fb2aa5af70eede4c723da7f6ba27d45
Sem <159899974+Sem437@users.noreply.github.com>
2024-09-03 11:51:25 +0200

    BoekModel gemaakt + migrations

---
+++ BoekWinkel/Models/BoekModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoekWinkel.Models
+{
+    public class BoekModel
+    {
+        public int BoekId { get; set; }
+
+        [Required]
+        public string BoekTitle { get; set; }
+
+        [Required]
+        public string BoekAuthor { get; set; }
+
+        [Required]
+        public string BoekDescription { get; set;}
+
+        [Required]
+        public int BoekPrice { get; set; }
+
+        [Required]
+        public string BoekCategory { get; set;}
+    
+    }
+}
