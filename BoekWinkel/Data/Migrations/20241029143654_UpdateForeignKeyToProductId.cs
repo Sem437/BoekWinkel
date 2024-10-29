e3c1ae379ee50aff0328800b72ab3c078903ea44
Sem <159899974+Sem437@users.noreply.github.com>
2024-10-29 16:15:49 +0100

    sttling en layout voor wishlist index gemaakt

---
+++ BoekWinkel/Data/Migrations/20241029143654_UpdateForeignKeyToProductId.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoekWinkel.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateForeignKeyToProductId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_VerlanglijstModel_ProductId",
+                table: "VerlanglijstModel",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VerlanglijstModel_BoekModel_ProductId",
+                table: "VerlanglijstModel",
+                column: "ProductId",
+                principalTable: "BoekModel",
+                principalColumn: "BoekId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VerlanglijstModel_BoekModel_ProductId",
+                table: "VerlanglijstModel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VerlanglijstModel_ProductId",
+                table: "VerlanglijstModel");
+        }
+    }
+}
